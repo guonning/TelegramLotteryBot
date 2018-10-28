cc37b71dc264c11be9f564ccf12e928ae9a5b315
WooMai <1640829150@qq.com>
2018-10-28 19:20:36 +0800

    乱七八糟的修改

--- details.php
+++ details.php
@@ -3,8 +3,8 @@ $uri = explode('/',$_SERVER['REQUEST_URI']);
 
 if(!isset($uri[3]) || !isset($uri[4]) || !file_exists("./sessions/details/$hash.json"))
 {
-	//header('HTTP/1.1 403 Forbidden');
-	//exit();
+	header('HTTP/1.1 403 Forbidden');
+	exit();
 }
 
 $number = $uri[3];
@@ -12,7 +12,7 @@ $hash = $uri[4];
 
 if(file_exists("./sessions/details/$hash.json"))
 {
-	$undefined = false;
+	//$undefined = false;
 	$json = file_get_contents("./sessions/details/$hash.json");
 	$data = json_decode($json);
 }
@@ -23,22 +23,13 @@ if(file_exists("./sessions/details/$hash.json"))
 	<title>Azuki Lottery Bot</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="https://azuki.cloud/favicon.ico"/>
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.loli.net/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.loli.net/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.loli.net/ajax/libs/animate.css/3.5.2/animate.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.loli.net/ajax/libs/select2/4.0.3/css/select2.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.4.0/css/perfect-scrollbar.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/LotteryBot/assets/css/util.css">
 	<link rel="stylesheet" type="text/css" href="/LotteryBot/assets/css/main.css">
-<!--===============================================================================================-->
 </head>
 <body>
 	
@@ -82,9 +73,6 @@ if(file_exists("./sessions/details/$hash.json"))
 			</div>
 		</div>
 	</div>
-
-
-	
 
 <!--===============================================================================================-->	
 	<script src="https://cdnjs.loli.net/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
