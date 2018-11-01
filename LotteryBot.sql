4bee9cb985c270393ba8a21c819fa09c64be3c50
WooMai <1640829150@qq.com>
2018-11-01 21:42:46 +0800

    update

--- LotteryBot.sql
+++ LotteryBot.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
 --
--- 主机： 52.193.196.168:3306
--- 生成日期： 2018-10-27 13:40:47
--- 服务器版本： 5.6.41-log
--- PHP 版本： 7.2.10
+-- Host: localhost
+-- Generation Time: 2018-11-01 21:41:36
+-- 服务器版本： 5.5.61-log
+-- PHP Version: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 数据库： `LotteryBot`
+-- Database: `LotteryBot`
 --
 
 -- --------------------------------------------------------
@@ -28,13 +26,14 @@ SET time_zone = "+00:00";
 -- 表的结构 `lottery_list`
 --
 
-CREATE TABLE `lottery_list` (
+CREATE TABLE IF NOT EXISTS `lottery_list` (
   `id` int(11) NOT NULL COMMENT 'id',
   `number` bigint(11) NOT NULL COMMENT '抽奖编号',
   `token` text NOT NULL COMMENT 'Join 抽奖令牌',
   `title` text NOT NULL COMMENT '抽奖标题',
   `details` mediumtext NOT NULL COMMENT '抽奖详情',
   `prize` int(11) NOT NULL COMMENT '奖品数',
+  `smart` tinyint(1) NOT NULL DEFAULT '0' COMMENT '智能概率控制',
   `req_uid` int(11) NOT NULL COMMENT '创建者 Telegram ID',
   `req_username` text NOT NULL COMMENT '创建者tg用户名',
   `req_firstname` text NOT NULL COMMENT '创建者 first name',
@@ -49,50 +48,48 @@ CREATE TABLE `lottery_list` (
 -- 表的结构 `lottery_tpl`
 --
 
-CREATE TABLE `lottery_tpl` (
+CREATE TABLE IF NOT EXISTS `lottery_tpl` (
   `id` int(11) NOT NULL COMMENT '自增id',
   `user_id` int(11) NOT NULL COMMENT 'Telegram User ID',
   `username` text NOT NULL,
   `first_name` text NOT NULL,
   `last_name` text NOT NULL,
+  `probability` decimal(10,8) NOT NULL DEFAULT '1.00000000' COMMENT '中奖率',
   `join_time` int(11) NOT NULL COMMENT '参与时间戳',
   `lang_code` text NOT NULL COMMENT 'language_code',
   `win` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否中奖'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 转储表的索引
+-- Indexes for dumped tables
 --
 
 --
--- 表的索引 `lottery_list`
+-- Indexes for table `lottery_list`
 --
 ALTER TABLE `lottery_list`
   ADD PRIMARY KEY (`id`,`number`);
 
 --
--- 表的索引 `lottery_tpl`
+-- Indexes for table `lottery_tpl`
 --
 ALTER TABLE `lottery_tpl`
   ADD PRIMARY KEY (`id`,`user_id`);
 
 --
--- 在导出的表使用AUTO_INCREMENT
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- 使用表AUTO_INCREMENT `lottery_list`
+-- AUTO_INCREMENT for table `lottery_list`
 --
 ALTER TABLE `lottery_list`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
-
 --
--- 使用表AUTO_INCREMENT `lottery_tpl`
+-- AUTO_INCREMENT for table `lottery_tpl`
 --
 ALTER TABLE `lottery_tpl`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id';
-COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
