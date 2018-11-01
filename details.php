6b61a037d3f838923ba124cb57bff06d1cdc8e88
WooMai <1640829150@qq.com>
2018-11-01 17:06:25 +0800

    改一个前端库的cdn

--- details.php
+++ details.php
@@ -29,7 +29,7 @@ if(file_exists("./sessions/details/$hash.json"))
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.loli.net/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.loli.net/ajax/libs/animate.css/3.5.2/animate.min.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.loli.net/ajax/libs/select2/4.0.3/css/select2.min.css">
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.4.0/css/perfect-scrollbar.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.loli.net/ajax/libs/jquery.perfect-scrollbar/1.4.0/css/perfect-scrollbar.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo $config['base_url'] ?>/assets/css/main.css">
 </head>
 <body>
