d0c6f2bf9cd8dd70a9084dac3920543b1471ea64
WooMai <1640829150@qq.com>
2018-11-04 00:06:21 +0800

    增加日志

--- commands/start.php
+++ commands/start.php
@@ -6,8 +6,8 @@ if(isset($cmd[1]))
     $c = GetDbConn();
     $token = $cmd[1];
     $prob = 1.00000000;
+    $timestamp = time();
 
-    //======================================================================
     $rs = $c->query("SELECT * FROM `lottery_list` WHERE `token` = '$token'");
 
     if($rs === false)
@@ -55,14 +55,34 @@ if(isset($cmd[1]))
         exit();
     }
 
-
     //==================== Smart Probability Control =======================
     if($smart != true) goto SmartOff;
 
+    if(!is_dir("./spc-log/$number/")) mkdir("./spc-log/$number/");
+
+    $log = "================ Smart Probability Control Log - Start ================\r\n".
+    "---------- User Info ----------\r\n".
+    "User ID: $from->id\r\n".
+    "Username: $from->username\r\n".
+    "First Name: $from->first_name\r\n".
+    "Last Name: $from->last_name\r\n".
+    "Language: $from->language_code\r\n".
+    "Time: ".date('Y-m-d H:i:s')."\r\n".
+    "Timestamp: $timestamp\r\n".
+    "Initial Probability: $prob\r\n\r\n".
+
+    "---------- Probability Calculation ----------\r\n";
+
     // username
+    $log .= 'Username: ';
     if($from->username == '')
     {
         $prob = $prob - 0.265;  // 如果未设置用户名
+        $log .= "Unset, Prob = $prob\r\n";
+    }
+    else
+    {
+        $log .= "Set, Prob = $prob\r\n";
     }
 
     // user id
@@ -70,6 +90,7 @@ if(isset($cmd[1]))
     if($from->id < 400000000) $prob = $prob + 0.132;
     if($from->id < 500000000) $prob = $prob + 0.096;
     if($from->id < 600000000) $prob = $prob + 0.072;
+    $log .= "UID: $from->id, Prob = $prob\r\n";
 
     // get profile picture
     $pic_info = TelegramAPI('getUserProfilePhotos',array('user_id'=>$from->id));
@@ -85,12 +106,16 @@ if(isset($cmd[1]))
     if($pic_count >= 7) $prob = $prob + 0.065;
     if($pic_count >= 2) $prob = $prob + 0.032;
     if($pic_count == 0) $prob = $prob - 0.435;
+    $log .= "Profile Photos Count: $pic_count, Prob = $prob\r\n\r\n";
 
     // Cloud Vision API
     if($config['enable_vision_api'] !== true || $pic_count == 0) goto VisionEnd;
+    $log .= "##### Start Google Cloud Vision API Checking #####\r\n";
+
     $photos = $pic_info->result->photos;
     $file_id = $photos[0][2]->file_id;
 
+    //$log .= "Uploading user's recent profile photo...\r\n";
     $vision = CloudVisionApi($file_id);
     if($vision === false)
     {
@@ -117,18 +142,21 @@ if(isset($cmd[1]))
     if($perfer1 !== false && $labels[$perfer1]->score >= 0.7)
     {
         $prob = $prob + (0.29 * $labels[$perfer1]->score);
+        $log .= "Detected profile photo containing \"Anime\", Prob = $prob\r\n";
     }
 
     // "cartoon" label
     if($perfer2 !== false && $labels[$perfer2]->score >= 0.72)
     {
         $prob = $prob + (0.22 * $labels[$perfer2]->score);
+        $log .= "Detected profile photo containing \"Cartoon\", Prob = $prob\r\n";
     }
 
     // illust
     if($perfer3 !== false && $labels[$perfer3]->score >= 0.63)
     {
         $prob = $prob + (0.1 * $labels[$perfer3]->score);
+        $log .= "Detected profile photo containing \"Illustration\", Prob = $prob\r\n";
     }
 
 
@@ -138,6 +166,7 @@ if(isset($cmd[1]))
     {
         case 'VERY_LIKELY':
         $prob = $prob - 0.05;
+        $log .= "\"Adult\" content of \"Very Likely\" detected, Prob = $prob\r\n";
         break;
     }
 
@@ -146,14 +175,17 @@ if(isset($cmd[1]))
     {
         case 'POSSIBLE':
         $prob = $prob - 0.11;
+        $log .= "\"Spoof\" content of \"Possible\" detected, Prob = $prob\r\n";
         break;
 
         case 'LIKELY':
         $prob = $prob - 0.23;
+        $log .= "\"Spoof\" content of \"Likely\" detected, Prob = $prob\r\n";
         break;
 
         case 'VERY_LIKELY':
         $prob = $prob - 0.46;
+        $log .= "\"Spoof\" content of \"Very Likely\" detected, Prob = $prob\r\n";
         break;
     }
 
@@ -162,14 +194,17 @@ if(isset($cmd[1]))
     {
         case 'POSSIBLE':
         $prob = $prob - 0.13;
+        $log .= "\"Medical\" content of \"Possible\" detected, Prob = $prob\r\n";
         break;
 
         case 'LIKELY':
         $prob = $prob - 0.24;
+        $log .= "\"Medical\" content of \"Likely\" detected, Prob = $prob\r\n";
         break;
 
         case 'VERY_LIKELY':
         $prob = $prob - 0.5;
+        $log .= "\"Medical\" content of \"Very Likely\" detected, Prob = $prob\r\n";
         break;
     }
 
@@ -178,24 +213,45 @@ if(isset($cmd[1]))
     {
         case 'POSSIBLE':
         $prob = $prob - 0.115;
+        $log .= "\"Violence\" content of \"Possible\" detected, Prob = $prob\r\n";
         break;
 
         case 'LIKELY':
         $prob = $prob - 0.235;
+        $log .= "\"Violence\" content of \"Likely\" detected, Prob = $prob\r\n";
         break;
 
         case 'VERY_LIKELY':
         $prob = $prob - 0.46;
+        $log .= "\"Violence\" content of \"Very Likely\" detected, Prob = $prob\r\n";
         break;
     }
 
-    //switch($safety->racy)  // 不存在的
+    // 不存在的
+    switch($safety->racy) 
+    {
+        case 'POSSIBLE':
+        $log .= "\"Racy\" content of \"Possible\" detected, but who cares? lol\r\n";
+        break;
+
+        case 'LIKELY':
+        $log .= "\"Racy\" content of \"Likely\" detected, but who cares? lol\r\n";
+        break;
+
+        case 'VERY_LIKELY':
+        $log .= "\"Racy\" content of \"Very Likely\" detected, but who cares? lol\r\n";
+        break;
+    }  
+
+    $log .= "\r\n";
 
     VisionEnd:
     $prob = round($prob,4);
+    $log .= "Final probability result: $prob\r\n\r\n".
+    "================ Smart Probability Control Log - End ================";
+    file_put_contents("./spc-log/$number/SPC-$timestamp-$from->first_name.txt",$log);
     SmartOff:
     //======================================================================
-    $timestamp = time();
     $rs = $c->query("INSERT INTO `$number` (`user_id`, `username`, `first_name`, `last_name`, `probability`, `join_time`, `lang_code`) VALUES ('$from->id', '$from->username', '$from->first_name', '$from->last_name', '$prob', '$timestamp', '$from->language_code')");
 
     if($rs === false)
